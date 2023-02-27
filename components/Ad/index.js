e6da6ad4b02b3f0ffdaca56400dcab6932e3f33e
Patricia Jiang <patriciaejiang@gmail.com>
2023-02-27 10:13:46 -0800

    initial commit

---
+++ components/Ad/index.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import styles from './Ad.module.css'; //the . means wherever you are right now, just stay in this folder and look for the following file
+
+export default function Ad() {
+
+    const [popup, setPopup] = useState(true)
+
+    return (
+        <>
+            {
+                popup ?
+                    <div className={styles.container}>
+                        <div className={styles.popup__area}>
+                            <div onClick={() => setPopup(false)}>Close</div>
+                            Welcome
+                        </div>
+                    </div>
+                    :<></>
+        }
+
+        </>
+    )
+}
